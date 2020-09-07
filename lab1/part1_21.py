fe19d197808ef29f178b5ed56a4aba2d32c9b3ea
Mar Balibrea <marbalibrea8@gmail.com>
2020-09-07 20:58:03 +0200

    name changes

---
+++ lab1/part1_21.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from delta import delta_rule_0hlayer_batch
+from extras import plot_error
+from perceptron import perceptron_rule_0hlayer_batch
+
+
+## 3.2.1 Classification of linearly non-separable data
